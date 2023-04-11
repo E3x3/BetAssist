9540937e24860127789576aa35bb2f44e5f0a4df
E3x3 <roush.ethan@gmail.com>
2023-04-11 14:21:05 -0700

    Added Prize Pick scraper (Scraper.py) and NBA Stat Collector to calculate which bets are good BetAssist.py)

---
+++ BetAssist.py
@@ -0,0 +1,99 @@
+import time
+from nba_api.stats.endpoints import playergamelog
+from Scraper import Scraper
+
+# GLOBALS
+POINT_IND = 24
+REB_IND = 18
+ASSIST_IND = 19
+
+
+class BetAssist:
+    def _getGames(self, playerID):
+        gamelogs = playergamelog.PlayerGameLog(playerID)
+        return gamelogs.get_dict()['resultSets'][0]['rowSet']
+
+    # TODO: take into account missed games
+    def _getPlayerStatlines(self, playerID, numGames):
+        games = self._getGames(playerID)
+        statlines = []
+        for i in range(0, numGames):
+            game_dict = {'pts': games[i][POINT_IND], 'asts': games[i][ASSIST_IND], 'rebs': games[i][REB_IND]}
+            statlines.append(game_dict)
+        return statlines
+
+    def _getPlayerTotals(self, playerID, numGames):
+        print(playerID)
+        playerTotals = {'Points': 0.0, 'Rebounds': 0.0, 'Assists': 0.0, 'Pts+Rebs+Asts': 0.0, 'Pts+Rebs': 0.0,
+                        'Pts+Asts': 0.0, 'Rebs+Asts': 0.0}
+        statlines = self._getPlayerStatlines(playerID, numGames)
+
+        for statline in statlines:
+            playerTotals['Points'] += statline['pts']
+            playerTotals['Pts+Rebs+Asts'] += statline['pts']
+            playerTotals['Pts+Rebs'] += statline['pts']
+            playerTotals['Pts+Asts'] += statline['pts']
+            playerTotals['Rebounds'] += statline['rebs']
+            playerTotals['Pts+Rebs'] += statline['rebs']
+            playerTotals['Pts+Rebs+Asts'] += statline['rebs']
+            playerTotals['Rebs+Asts'] += statline['rebs']
+            playerTotals['Assists'] += statline['asts']
+            playerTotals['Pts+Rebs+Asts'] += statline['asts']
+            playerTotals['Pts+Asts'] += statline['asts']
+            playerTotals['Rebs+Asts'] += statline['asts']
+
+        player_totals = {key: value / numGames for key, value in playerTotals.items()}
+
+        return player_totals
+
+    # betData must be in the format: [(playerID, overNum, category), ...]
+    # where overNum is the over/under and category is the statistical combination
+    # of the over/under (pts, rebs, asts, pts_asts_rebs, pts_rebs, pts_asts, rebs_asts)
+    # betThreshold is a float between 0.0 and 1.0
+    def findGoodBets(self, betData, overBetThreshold, underBetThreshold, numGames):
+        goodBetList = []
+        for i in range(len(betData)):
+            if i % 10 == 0:
+                time.sleep(5)
+            entry = betData[i]
+            playerID = entry['id']
+            overNum = float(entry['Over'])
+            category = entry['Prop']
+
+            playerTotals = self._getPlayerTotals(playerID, numGames)
+            playerStat = playerTotals[category]
+
+            overageAmount = playerStat / overNum - 1
+
+            underageAmount = 1 - playerStat / overNum
+
+            if overageAmount > overBetThreshold:
+                betInfo = list(entry)
+                betInfo.append(round(overageAmount, 3))
+                betInfo.append("TAKE THE OVER ")
+                goodBetList.append(betInfo)
+
+            if underageAmount > underBetThreshold:
+                betInfo = list(entry)
+                betInfo.append(round(underageAmount, 3))
+                betInfo.append("TAKE THE UNDER ")
+                goodBetList.append(betInfo)
+
+        return goodBetList
+
+
+if __name__ == '__main__':
+    NBAScraper = Scraper()
+    betData = NBAScraper.scrapeNBAProps()
+    betAssist = BetAssist()
+    print(betAssist.findGoodBets(betData, 0.1, 0.2, 5))
+
+'''
+
+'''
+
+'''
+               (1629027, 37.5, 'pts_asts_rebs'), (202710, 40.5, 'pts_asts_rebs'), (1627749, 29.5, 'pts_asts_rebs'), (1628389, 32.5, 'pts_asts_rebs'),
+               (203991, 20.5, 'pts_asts_rebs'), (1629639, 29.5, 'pts_asts_rebs'), (201942, 32.5, 'pts_asts_rebs'), (1627832, 31.5, 'pts_asts_rebs'),
+               (203897, 34.5, 'pts_asts_rebs'), (1627783, 36.5, 'pts_asts_rebs')]
+'''
---
+++ Scraper.py
@@ -0,0 +1,67 @@
+# https://www.actowizsolutions.com/scrape-sports-betting-props-from-prizepicks.php
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import pandas as pd
+from nba_api.stats.static import players
+
+
+class Scraper:
+    def scrapeNBAProps(self):
+        PATH = "/Users/ethanroush/Documents/chromedriver_mac64/chromedriver"
+        driver = webdriver.Chrome(PATH)
+
+        driver.get("https://app.prizepicks.com/")
+
+        # wait = WebDriverWait(driver, 15).until(EC.presence_of_element_located(By.C))
+        time.sleep(1)
+        driver.find_element(By.XPATH, "/html/body/div[2]/div[3]/div/div/div[3]/button").click()
+        time.sleep(1)
+
+        ppPlayers = []
+
+        driver.find_element(By.XPATH, "//div[@class='name'][normalize-space()='NBA']").click()
+        time.sleep(2)
+
+        # stats_container = WebDriverWait(driver, 1).until(EC.visibility_of_element_located())
+        time.sleep(2)
+
+        categories = driver.find_element(By.CSS_SELECTOR, ".stat-container").text.split("\n")
+
+        for category in categories:
+            if (category == 'Points' or category == 'Rebounds' or category == 'Assists'
+                    or category == 'Pts+Rebs+Asts' or category == 'Pts+Rebs'
+                    or category == 'Pts+Asts' or category == 'Rebs+Asts'):
+                print("doing category: " + category)
+                driver.find_element(By.XPATH, f"//div[text()='{category}']").click()
+
+                projectionsPP = WebDriverWait(driver, 5).until(
+                    EC.presence_of_all_elements_located((By.CSS_SELECTOR, ".projection"))
+                )
+
+                for projections in projectionsPP:
+                    names = projections.find_element(By.CLASS_NAME, "name").text
+                    pts = projections.find_element(By.CLASS_NAME, "presale-score").get_attribute("textContent")
+                    prototype = projections.find_element(By.CLASS_NAME, "text").get_attribute("textContent")
+
+                    playerInfo = {
+                        'Name': names,
+                        'Over': pts,
+                        'Prop': category
+                    }
+
+                    ppPlayers.append(playerInfo)
+
+        ppPlayersTrimmed = []
+        for player in ppPlayers:
+            player_name = player['Name']
+            playerlist = list(players.find_players_by_full_name(player_name))
+            if len(playerlist) != 0:
+                newPlayer = player
+                newPlayer['id'] = playerlist[0]['id']
+                ppPlayersTrimmed.append(newPlayer)
+
+        return ppPlayersTrimmed
